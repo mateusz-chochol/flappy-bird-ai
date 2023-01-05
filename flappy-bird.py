ccb6f9270e1af508f78f9a49082f399892d84d9a
Mateusz Chochol <mch.priv@gmail.com>
2023-01-05 09:59:05 +0100

    Adding for set of trained genomes

--- flappy-bird.py
+++ flappy-bird.py
@@ -1,38 +1,39 @@
-import pygame
-pygame.font.init()
 
-import neat
-import os
-import sys
-
-from utils.pickle_utils import save_genome, load_genome
-from main.standalone_game import run_standalone_game
+import pygame  # noqa
+pygame.font.init()  # noqa
 from main.neural_networks.feed_forward_neural_network import learn_feed_forward_network, run_trained_feed_forward_network
+from main.standalone_game import run_standalone_game
+from utils.pickle_utils import save_genome, load_genome
+import sys
+import os
+import neat
 
 
 if __name__ == "__main__":
-  if len(sys.argv) > 1:
-    local_dir = os.path.dirname(__file__)
-    genomes_dir = os.path.join(local_dir, "saved_genomes")
-    config_path = os.path.join(local_dir, "config/neat-config.txt")
+    if len(sys.argv) > 1:
+        local_dir = os.path.dirname(__file__)
+        genomes_dir = os.path.join(local_dir, "saved_genomes")
+        config_path = os.path.join(local_dir, "config/neat-config.txt")
 
-    neat_config = neat.Config(neat.DefaultGenome, neat.DefaultReproduction, neat.DefaultSpeciesSet, neat.DefaultStagnation, config_path)
+        neat_config = neat.Config(neat.DefaultGenome, neat.DefaultReproduction,
+                                  neat.DefaultSpeciesSet, neat.DefaultStagnation, config_path)
 
-    if sys.argv[1] == "learn":
-      population = neat.Population(neat_config)
-      population.add_reporter(neat.StdOutReporter(True))
-      population.add_reporter(neat.StatisticsReporter())
-      winner_model = population.run(learn_feed_forward_network, 200)
+        if sys.argv[1] == "learn":
+            population = neat.Population(neat_config)
+            population.add_reporter(neat.StdOutReporter(True))
+            population.add_reporter(neat.StatisticsReporter())
+            winner_model = population.run(learn_feed_forward_network, 20)
 
-      print(f"Best genome: {winner_model}")
-      save_genome(winner_model, "feed_forward_best_genome_random_200_generations.sav", genomes_dir)
-    elif sys.argv[1] == "run_trained":
-      # add checks if file exists
-      best_genome = load_genome("feed_forward_best_genome_random_200_generations.sav", genomes_dir)
-      run_trained_feed_forward_network(best_genome, neat_config)
-  else:
-    while True:
-      run_standalone_game()
+            print(f"Best genome: {winner_model}")
+            save_genome(
+                winner_model, "feed_forward_best_genome_no_randomness_20_generations.sav", genomes_dir)
+        elif sys.argv[1] == "run_trained":
+            # add checks if file exists
+            best_genome = load_genome(
+                "feed_forward_best_genome_no_randomness_20_generations.sav", genomes_dir)
+            run_trained_feed_forward_network(best_genome, neat_config)
+    else:
+        while True:
+            run_standalone_game()
 
 # Refactor to add types
-# Refactor to add some prettier so the indentation is 4 spaces instead of 2
--- models/pipe.py
+++ models/pipe.py
@@ -5,52 +5,58 @@ import utils.constants as consts
 
 rng = random.Random(42)
 
+
 class Pipe:
-  GAP_BETWEEN_PIPES = 220
-  VELOCITY = 5
+    GAP_BETWEEN_PIPES = 220
+    VELOCITY = 5
 
-  def __init__(self, starting_x):
-    self.x = starting_x
-    self.gap_height = 0
-    self.top_pipe_y = 0
-    self.bottom_pipe_y = 0
-    self.current_y_displacement = 0
-    self.BOTTOM_PIPE_IMG = consts.PIPE_IMG
+    def __init__(self, starting_x):
+        self.x = starting_x
+        self.gap_height = 0
+        self.top_pipe_y = 0
+        self.bottom_pipe_y = 0
+        self.current_y_displacement = 0
+        self.BOTTOM_PIPE_IMG = consts.PIPE_IMG
 # maybe refactor it so its not being transformed with every pipe
-    self.TOP_PIPE_IMG = pygame.transform.flip(self.BOTTOM_PIPE_IMG, False, True)
+        self.TOP_PIPE_IMG = pygame.transform.flip(
+            self.BOTTOM_PIPE_IMG, False, True)
 
-    self.has_been_passed = False
-    self.set_height()
+        self.has_been_passed = False
+        self.set_height()
 
-  def set_height(self):
-    self.gap_height = random.randrange(50, 450)
-    # self.gap_height = rng.randrange(50, 450)
-    self.bottom_pipe_y = self.gap_height + self.GAP_BETWEEN_PIPES
-    self.top_pipe_y = self.gap_height - self.TOP_PIPE_IMG.get_height()
+    def set_height(self):
+        # self.gap_height = random.randrange(50, 450)
+        self.gap_height = rng.randrange(50, 450)
+        self.bottom_pipe_y = self.gap_height + self.GAP_BETWEEN_PIPES
+        self.top_pipe_y = self.gap_height - self.TOP_PIPE_IMG.get_height()
 
-  def move(self):
-    self.x -= self.VELOCITY
+    def move(self):
+        self.x -= self.VELOCITY
 
-    self.current_y_displacement = (-1 if math.sin((self.x - self.gap_height)/ 100) > 0 else 1) * 2
-    self.bottom_pipe_y -= self.current_y_displacement
-    self.top_pipe_y -= self.current_y_displacement
+        self.current_y_displacement = (-1 if math.sin(
+            (self.x - self.gap_height) / 100) > 0 else 1) * 2
+        self.bottom_pipe_y -= self.current_y_displacement
+        self.top_pipe_y -= self.current_y_displacement
 
-  def draw(self, window):
-    window.blit(self.BOTTOM_PIPE_IMG, (self.x, self.bottom_pipe_y))
-    window.blit(self.TOP_PIPE_IMG, (self.x, self.top_pipe_y))
+    def draw(self, window):
+        window.blit(self.BOTTOM_PIPE_IMG, (self.x, self.bottom_pipe_y))
+        window.blit(self.TOP_PIPE_IMG, (self.x, self.top_pipe_y))
 
-  def is_colliding(self, bird):
-    bird_mask = bird.get_mask()
-    bottom_pipe_mask = pygame.mask.from_surface(self.BOTTOM_PIPE_IMG)
-    top_pipe_mask = pygame.mask.from_surface(self.TOP_PIPE_IMG)
+    def is_colliding(self, bird):
+        bird_mask = bird.get_mask()
+        bottom_pipe_mask = pygame.mask.from_surface(self.BOTTOM_PIPE_IMG)
+        top_pipe_mask = pygame.mask.from_surface(self.TOP_PIPE_IMG)
 
-    bottom_pipe_offset = (self.x - bird.x, self.bottom_pipe_y - round(bird.y))
-    top_pipe_offset = (self.x - bird.x, self.top_pipe_y - round(bird.y))
+        bottom_pipe_offset = (
+            self.x - bird.x, self.bottom_pipe_y - round(bird.y))
+        top_pipe_offset = (self.x - bird.x, self.top_pipe_y - round(bird.y))
 
-    bottom_pipe_point_of_collision = bird_mask.overlap(bottom_pipe_mask, bottom_pipe_offset)
-    top_pipe_point_of_collision = bird_mask.overlap(top_pipe_mask, top_pipe_offset)
+        bottom_pipe_point_of_collision = bird_mask.overlap(
+            bottom_pipe_mask, bottom_pipe_offset)
+        top_pipe_point_of_collision = bird_mask.overlap(
+            top_pipe_mask, top_pipe_offset)
 
-    if bottom_pipe_point_of_collision or top_pipe_point_of_collision:
-      return True
+        if bottom_pipe_point_of_collision or top_pipe_point_of_collision:
+            return True
 
-    return False
+        return False
